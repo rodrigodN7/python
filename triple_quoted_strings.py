385ce3d903b556d5fdbfd2ea44031a72ccb93c04
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-21 10:04:13 -0500

    triple_quoted_strings

---
+++ triple_quoted_strings.py
@@ -0,0 +1,20 @@
+#
+#By Rodrigo Noriega
+#
+#
+#Triple-quoted strings
+#
+#
+
+print('Display "hi" in quotes')
+print('Display \'hi\' in quotes')
+print("Display the name O'Brien")
+print("Display \"hi\" in quotes")
+
+#to avoid using \' and \" inside strings, you can enclose such strings in triple quotes:
+print(""" Display "hi" and 'bye' in quotes """)
+
+#multiline triple-quoted string:
+triple_quoted_string = """This is a triple-quoted
+string that spans two lines"""
+print(triple_quoted_string)
