a00d2583b15350ff899f44967aa6940cb797ebdb
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-21 09:59:06 -0500

    print_and_single_quoted_srtrings

---
+++ print_and_single_double_quoted_strings.py
@@ -0,0 +1,30 @@
+#
+#By Rodrigo Noriega
+#
+#
+#Function print and an intro to sigle and
+#double-quoted strings
+#
+
+print('Welcome to Python!')
+print("Welcome to Python!")
+
+#comma-separated list of arguments
+print('Welcome', 'to', 'Python!')
+
+#the \n new line character
+print('Welcome\nto\npython!')
+
+#Escape sequences
+#\n, insert new line
+#\t, insert horizontal tab
+#\\, insert backslah character in a string
+#\", insert a double quote character in a string
+#\'. insert a single quote character in a string
+
+#ignoring a Line Brake in a Long String
+print('this is a longer string, so we\
+ split it over two lines')
+
+ #printing the value of an expression
+print('Sum is', 7 + 3)
