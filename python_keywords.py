406b90938feb909335d040c6ae5b58698dc102a1
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-21 20:18:57 -0500

    python_keywords

---
+++ python_keywords.py
@@ -0,0 +1,19 @@
+#
+#By Rodrigo Noriega
+#
+#
+#Python keywords
+#
+#
+
+python_keywords = [
+                   'and', 'as', 'assert', 'async', 'await',
+                   'break', 'class', 'continue', 'def', 'del',
+                   'elif', 'else', 'except', 'False', 'finally'
+                   'for', 'from', 'global', 'if', 'import',
+                   'in', 'is', 'lambda', 'None', 'nonlocal',
+                   'not', 'or', 'pass', 'raise', 'return',
+                   'True', 'try', 'while', 'with', 'yield'
+                    ]
+
+print(python_keywords)
