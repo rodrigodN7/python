4e7893c0dfc318d7622163e8807596888cf89592
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-21 20:02:40 -0500

    bitwise_operators

---
+++ bitwise_operators.py
@@ -0,0 +1,35 @@
+#
+#By Rodrigo Noriega
+#
+#
+#Bitwise operators
+#
+#
+
+a = 60            # 60 = 0011 1100 
+b = 13            # 13 = 0000 1101 
+c = 0
+
+#and
+c = a & b;        # 12 = 0000 1100
+print("Line 1 - Value of c is ", c)
+
+#or
+c = a | b;        # 61 = 0011 1101 
+print("Line 2 - Value of c is ", c)
+
+#xor
+c = a ^ b;        # 49 = 0011 0001
+print("Line 3 - Value of c is ", c)
+
+#binary ones complement
+c = ~a;           # -61 = 1100 0011
+print("Line 4 - Value of c is ", c)
+
+#binary left shift
+c = a << 2;       # 240 = 1111 0000
+print("Line 5 - Value of c is ", c)
+
+#binary right shift
+c = a >> 2;       # 15 = 0000 1111
+print("Line 6 - Value of c is ", c)
