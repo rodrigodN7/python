439439bb300f2916df96d4f003a9613ef3468456
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-21 21:28:25 -0500

    for_statement

---
+++ for_statement.py
@@ -0,0 +1,38 @@
+#
+#By Rodrigo Noriega
+#
+#
+#for statement
+#
+#
+
+for character in 'Programming':
+    print(character, end='  ')
+
+print(10, 20, 30, sep=', ')
+
+#iterables, lists and iterators
+
+total = 0
+for number in [2, -3, 0, 17, 9]:
+    total = total + number
+
+print(total)
+
+#built in range function
+for counter in range(10):
+    print(counter, end=' ')
+
+#augmented assignments
+for number in [1,2,3,4,5]:
+    total += number #is the same as total = total + number
+
+#list of the augmented assignments
+
+augmented_assignment = [
+                        '+=', '-=', '*=',
+                        '**=', '/=', '//=',
+                        '%='
+                        ]
+
+print(augmented_assignment)
