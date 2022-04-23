9767533253ce1547a1fa66faf4185c8f3bb6beb4
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-23 17:24:02 -0500

    math_module

---
+++ math_module.py
@@ -0,0 +1,47 @@
+#
+#By Rodrigo Noriega
+#
+#A game of chance
+#
+#
+#
+
+import math
+print(dir(math))
+print(math.sqrt(900))
+
+#calculate the absolute value of -10
+print(math.fabs(-10))
+
+#rounds x to the smallest integer not less than x
+print(math.ceil(9.2))
+print(math.ceil(9.6))
+
+#rounds x to the largest integer not grater than x
+print(math.floor(9.2))
+print(math.floor(9.6))
+
+#trigonometric sine of x (x in radians)
+print(math.sin(0))
+#trigonometric cosine of x (x in radians)}
+print(math.cos(0))
+#trigonometric tangent of x (x in radians)}
+print(math.tan(0))
+#exponential function e^x
+print(math.exp(1))
+print(f'{math.exp(1):.2f}')
+#natural logarithm of x (base e)
+print(math.log(7.389056))
+#logarithm of x (base 10)
+print(math.log10(10))
+# power x^y
+print(math.pow(2,7))
+#square root of x
+print(math.sqrt(900))
+#Absolute value of x—always returns a float.
+#Python also has the builtin
+#function abs, which returns an int or a float, based on
+#its argument.
+print(math.fabs(5.1))
+#Remainder (modulo) of x/y as a floatingpoint number
+print(math.fmod(9.8,4.0))
