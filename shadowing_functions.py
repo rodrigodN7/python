d7bbd49bd2005230f9fa5f05b5f63cf581b3e748
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-23 22:20:28 -0500

    shadowing_functions

---
+++ shadowing_functions.py
@@ -0,0 +1,22 @@
+#
+#By Rodrigo Noriega
+#
+#Methods: functions that belongs to objects
+#
+#
+#
+
+#If you define avariable named sum, it shadows the builtin
+#function, making it inaccessible in your code.
+
+#remember, sum is a built-in function
+#if you declare it as a variable, the identifier
+#no longer references the built-in function
+
+print(sum([10,5]))
+
+sum = 10 + 5
+print(sum)
+
+#this will show an error
+print(sum([10,5]))
