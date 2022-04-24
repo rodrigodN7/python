5d7de8e5c5295687379fffa56bd137ba9e2688ee
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-24 02:19:28 -0500

    measures_of_dispersion

---
+++ measures_of_dispersion.py
@@ -0,0 +1,17 @@
+#
+#By Rodrigo Noriega
+#
+#Measures of dispersion
+#
+#
+#
+
+import statistics
+import math
+
+#variance
+print(statistics.variance([1,3,4,2,6,5,3,4,5,2]))
+
+#standard deviation
+print(statistics.pstdev([1,3,4,2,6,5,3,4,5,2]))
+print(math.sqrt(statistics.pvariance([1,3,4,2,6,5,3,4,5,2])))
