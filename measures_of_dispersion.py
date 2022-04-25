ebf4c2206a83721a138c4b4ea96134ead316a34d
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-24 21:08:24 -0500

    measures_of_dispersion

--- measures_of_dispersion.py
+++ measures_of_dispersion.py
@@ -14,4 +14,5 @@ print(statistics.variance([1,3,4,2,6,5,3,4,5,2]))
 
 #standard deviation
 print(statistics.pstdev([1,3,4,2,6,5,3,4,5,2]))
+#Passing the pvariance function’s result to the math module’s sqrt function confirms our result
 print(math.sqrt(statistics.pvariance([1,3,4,2,6,5,3,4,5,2])))
