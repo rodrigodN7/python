6deff4d71305854cf30d7cfe8cbf6362c024fdbd
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-25 18:39:00 -0500

    searching_sequences

--- searching_sequences.py
+++ searching_sequences.py
@@ -32,9 +32,18 @@ print(5 in numbers)
 print(1000 not in numbers)
 print(5 not in numbers)
 
+#Using Operator in to Prevent a ValueError
 key = 1000
 search_key = 3
 if key in numbers:
     print(f'found value {key} at index {numbers.index(search_key)}') #search key is the key of list where we want to search
 else:
     print(f'value {key} not found')
+
+#Built-In Functions any and all
+#Sometimes you simply need to know whether any item in an iterable is True or
+#whether all the items are True. The builtin function any returns True if any item in
+#its iterable argument is True. The builtin function all returns True if all items in its
+#iterable argument are True. Recall that nonzero values are True and 0 is False. Nonempty
+#iterable objects also evaluate to True, whereas any empty iterable evaluates toFalse. 
+# Functions any and all are additional examples of internal iteration infunctionalstyle programming.
