e9e1103e1feac5996d2876af4d86eb099644181b
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-04-29 23:22:06 -0500

    generator_expressions

---
+++ generator_expressions.py
@@ -0,0 +1,20 @@
+#
+#By Rodrigo Noriega
+#
+#generator expressions
+#
+#
+#
+
+#generator expressions can reduce your program’s
+#memory consumption and improve performance if the whole list is not needed at once.
+
+numbers = [10, 3, 7, 1, 9, 4, 2, 8, 5, 6]
+
+for value in (x ** 2 for x in numbers if x % 2 != 0):
+    print(value, end=' ')
+
+#To show that a generator expression does not create a list, let’s assign the preceding
+#snippet’s generator expression to a variable and evaluate the variable:
+squares_of_odds = (x ** 2 for x in numbers if x % 2 != 0)
+print('\n', squares_of_odds)
