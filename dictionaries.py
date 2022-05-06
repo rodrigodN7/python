22a261ce05b5c14ae2345376f5782f2a07c61695
rodrigodN7 <102780110+rodrigodN7@users.noreply.github.com>
2022-05-05 21:45:43 -0500

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -64,3 +64,114 @@ print(roman_numerals.get('V'))
 print('V' in roman_numerals)
 print('III' in roman_numerals)
 print('III' not in roman_numerals)
+
+#dictionary methods keys and values
+months = {'January':1, 'February':2, 'March':3}
+
+for month_name in months.keys():
+    print(month_name, end=' ')
+
+for month_number in months.values():
+    print(month_number, end=' ')
+
+#dictionary views
+months_view = months.keys()
+
+for key in months_view:
+    print(key, end = ' ')
+
+months['December'] = 12 
+print(months)
+
+for key in months_view:
+    print(key, end = ' ')
+
+#converting dictionary keys, values and key-value pairs to lists
+print(list(months.keys()))
+print(list(months.values()))
+print(list(months.items()))
+
+#processing keys in sorted order
+for month_name in sorted(months.keys()):
+    print(month_name, end=' ')
+
+#dictionary comparisons
+country_capitals1 = {'Belgium': 'Brussels', 'Haiti': 'Port-au-Prince'}
+country_capitals2 = {'Nepal': 'Kathmandu', 'Uruguay': 'Montevideo'}
+country_capitals3 = {'Haiti': 'Port-au-Prince', 'Belgium': 'Brussels'}
+print(country_capitals1 == country_capitals2)
+print(country_capitals1 == country_capitals3)
+print(country_capitals1 != country_capitals2)
+
+#e.g. dictionary of students grade
+"""Using a dictionary to represent an instructor's grade book."""
+grade_book = {
+
+        'Susan': [92,85,100],
+        'Eduardo': [83,95,79],
+        'Azizi': [91, 89, 82],
+        'Pantipa': [97,91,92]
+}
+
+all_grades_total = 0
+all_grades_count = 0
+
+for name,grades in grade_book.items():
+    total = sum(grades)
+    print(f'Average for {name} is {total / len(grades):.2f}')
+    all_grades_total += total
+    all_grades_count += len(grades)
+
+print(f"Class's average is: {all_grades_total / all_grades_count:.2f}")
+
+#e.g word counts
+"""Tokenizing a string and counting unique words"""
+
+text = ('this is a sample text with several words'
+        'this is more sample text with some different words')
+
+word_counts = {}
+
+# count occurrences of each unique word
+for word in text.split():
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+
+print(f'{"WORD:<12"}COUNT')
+
+for word,count in sorted(word_counts.items()):
+    print(f'{word:<12}{count}')
+
+print('\nNumber of unique words:', len(word_counts))
+
+#python standard library module collections
+from collections import Counter
+
+text = ('this is sample text with several words'
+        'this is more sample twxt with some different words')
+
+counter = Counter(text.split())
+
+for word, count in sorted(counter.items()):
+    print(f'{word:<12}{count}')
+
+#dictionary method update
+country_code = {}
+country_code.update({'South Africa': 'za'})
+print(country_code)
+#Method update can convert keyword arguments into key–value pairs to insert.
+country_code.update({'Australia': 'ar'})
+print(country_code)
+#Method update also can receive an iterable object containing key–value pairs, such as
+#a list of twoelement tuples.
+country_code.update(({'Mexico': 'mx', 'United States': 'us'}))
+print(country_code)
+#dictionary comprehensions
+months = {'January':1, 'February':2, 'March':3}
+months2 = {number:name for name, number in months.items()}
+print(months2)
+grade = {'Sue':[98,87,94], 'Bob':[84,95,91]}
+grade2 = {k : sum(v) / len(v) for k, v in grade.items()}
+print(grade2)
